d69606b593c29a946efafaf6e688b5dedf4bb846
Joe Lawson <joelawson@JoenoMacBook-Pro.local>
2024-10-18 18:21:42 +0900

    initial commit

---
+++ day1/problem1/Problem1_1.java
@@ -0,0 +1,11 @@
+package day1.problem1;
+
+public class Problem1_1 {
+    /*
+     * Javaを使った最も基本的なプログラム
+     */
+    public static void main(String[] args) {
+        System.out.println("Joe Lawson"); // 「Hello,World.」という文字列を表示します。
+    }
+
+}
